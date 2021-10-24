0b77e6fcd3a511bbb99fbff2c448867b47a2f781
Robert Hambrock <roberthambrock@gmail.com>
2021-10-24 15:33:46 +0200

    finish renaming

--- scripts/generate-bindings.sh
+++ scripts/generate-bindings.sh
@@ -3,4 +3,4 @@
 $SOLC_BIN --abi ethereum/contracts/SwapDLEQ.sol -o ethereum/abi/ --overwrite
 $SOLC_BIN --bin ethereum/contracts/SwapDLEQ.sol -o ethereum/bin/ --overwrite
 abigen --abi ethereum/abi/SwapDLEQ.abi --pkg swapDLEQ --type SwapDLEQ --out swapDLEQ.go --bin ethereum/bin/SwapDLEQ.bin
-mv swapDLEQ.go ./swap-contract
+mv swapDLEQ.go ./swapDLEQ-contract
