570c3869e602961e7659c293a95ae95fab9420dd
Robert Hambrock <roberthambrock@gmail.com>
2021-10-24 02:34:14 +0200

    use cli args for privkey & filename

--- src/generate.rs
+++ src/generate.rs
@@ -1,6 +1,8 @@
 use farcaster_core::crypto::dleq::DLEQProof;
 use farcaster_core::consensus::CanonicalBytes;
+use std::convert::TryInto;
 use std::env;
+extern crate hex;
 
 fn zeroize_highest_bits(x: [u8; 32], highest_bit: usize) -> [u8; 32] {
     let mut x = x;
@@ -22,18 +24,21 @@ fn zeroize_highest_bits(x: [u8; 32], highest_bit: usize) -> [u8; 32] {
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    use rand::Rng;
-    let x: [u8; 32] = rand::thread_rng().gen();
-    let x_shaved = zeroize_highest_bits(x, 252);
-    let dleq = DLEQProof::generate(x_shaved);
+    // use rand::Rng;
+    // let x: [u8; 32] = rand::thread_rng().gen();
+    // let x_shaved = zeroize_highest_bits(x, 252);
+    let x: [u8; 32] = hex::decode(args.iter().nth(1).unwrap()).expect("Decoding failed").try_into().unwrap();
+    // let x: [u8; 32] = bytes!(args.first().unwrap());
+    let dleq = DLEQProof::generate(x);
 
     let bytes = dleq.as_canonical_bytes();
 
     use std::fs;
     use std::io::prelude::*;
 
-    let filename = "dleq_proof.dat";
+    let filename = args.iter().nth(2).unwrap();
     let file = fs::File::create(filename);
     file.unwrap().write_all(bytes.as_slice()).unwrap();
+    println!("successfully wrote dleq_proof to {:?}", filename);
     Ok(())
 }
