31cd6153713803f2e0dbb56efea6c8e26e18bb07
Robert Hambrock <roberthambrock@gmail.com>
2021-10-24 02:02:08 +0200

    verify DLEQ and print pubkeys

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ fn _zeroize_highest_bits(x: [u8; 32], highest_bit: usize) -> [u8; 32] {
     x
 }
 
+// fn main() -> Result<([u8; 32], [u8; 33]), std::io::Error> {
 // fn main() -> std::io::Result<()> {
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -51,7 +52,15 @@ fn main() {
     f.read(&mut buffer).expect("buffer overflow");
     let dleq2 = DLEQProof::from_canonical_bytes(bytes.as_slice()).unwrap();
 
-    // let commitment_agg_ed25519 = dleq2.c_g.iter().sum();
+    let commitment_agg_ed25519 = dleq2.c_g.iter().sum();
+    let commitment_agg_secp256k1 = dleq2
+        .c_h
+        .iter()
+        .fold(secp256k1Point::zero(), |acc, bit_commitment| {
+            g!(acc + bit_commitment).mark::<Normal>()
+        });
 
-    // dleq2.verify()
+    let verification = dleq2.verify(commitment_agg_ed25519, commitment_agg_secp256k1.mark::<NonZero>().unwrap()).unwrap();
+    println!("DLEQ proof successfully verified for:\ned25519:{:?}\nsecp256k1{:?}", commitment_agg_ed25519.compress().as_bytes(), commitment_agg_secp256k1.mark::<NonZero>().unwrap().to_bytes())
+    // Ok((*commitment_agg_ed25519.compress().as_bytes(), commitment_agg_secp256k1.mark::<NonZero>().unwrap().to_bytes()))
 }
