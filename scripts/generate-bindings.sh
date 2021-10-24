bb3536add730fc9959e00530b97086a874c385c4
Robert Hambrock <roberthambrock@gmail.com>
2021-10-24 15:49:10 +0200

    rename Swap->SwapOnChain

--- scripts/generate-bindings.sh
+++ scripts/generate-bindings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$SOLC_BIN --abi ethereum/contracts/Swap.sol -o ethereum/abi/ --overwrite
-$SOLC_BIN --bin ethereum/contracts/Swap.sol -o ethereum/bin/ --overwrite
-abigen --abi ethereum/abi/Swap.abi --pkg swap --type Swap --out swap.go --bin ethereum/bin/Swap.bin
-mv swap.go ./swap-contract
+$SOLC_BIN --abi ethereum/contracts/SwapOnChain.sol -o ethereum/abi/ --overwrite
+$SOLC_BIN --bin ethereum/contracts/SwapOnChain.sol -o ethereum/bin/ --overwrite
+abigen --abi ethereum/abi/SwapOnChain.abi --pkg swapOnChain --type SwapOnChain --out swapOnChain.go --bin ethereum/bin/SwapOnChain.bin
+mv swapOnChain.go ./swapOnChain-contract
