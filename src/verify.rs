964b676945d0a80b685865259fa19c055f159f3d
Robert Hambrock <roberthambrock@gmail.com>
2021-10-24 09:56:52 +0200

    demonstrate failure if dleq proof invalid by parsing invalid proof

--- src/verify.rs
+++ src/verify.rs
@@ -1,7 +1,7 @@
 use farcaster_core::crypto::dleq::DLEQProof;
 use farcaster_core::consensus::CanonicalBytes;
 use std::env;
-use ecdsa_fun::fun::Point as secp256k1Point;
+use ecdsa_fun::fun::{Point as secp256k1Point, Scalar as secp256k1Scalar, G};
 use secp256kfun::{g, marker::*};
 
 fn main() {
@@ -16,7 +16,13 @@ fn main() {
     let mut buffer = vec![0; metadata.len() as usize];
 
     f.read(&mut buffer).expect("buffer overflow");
-    let dleq = DLEQProof::from_canonical_bytes(buffer.as_slice()).unwrap();
+    let mut dleq = DLEQProof::from_canonical_bytes(buffer.as_slice()).unwrap();
+
+    let x_secp256k1: secp256k1Scalar<_> =
+        secp256k1Scalar::from_bytes_mod_order([255u8; 32])
+        .mark::<NonZero>()
+        .expect("x is zero");
+    *dleq.c_h.get_mut(0).unwrap() = g!(x_secp256k1 * G).mark::<Normal>();
 
     let commitment_agg_ed25519 = dleq.c_g.iter().sum();
     let commitment_agg_secp256k1 = dleq
