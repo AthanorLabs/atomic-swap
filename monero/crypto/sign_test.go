6b6c30cf18cd7a893424ba1f541ea4173ea3776e
noot <elizabethjbinks@gmail.com>
2021-12-11 19:56:09 -0500

    add sig hex test case

--- monero/crypto/sign_test.go
+++ monero/crypto/sign_test.go
@@ -28,3 +28,17 @@ func TestPrivateSpendKey_Verify(t *testing.T) {
 	ok := kp.sk.Public().Verify(msg, sig)
 	require.True(t, ok)
 }
+
+func TestSignature_Hex(t *testing.T) {
+	kp, err := GenerateKeys()
+	require.NoError(t, err)
+
+	msg := []byte("testmessage")
+	sig, err := kp.sk.Sign(msg)
+	require.NoError(t, err)
+
+	hex := sig.Hex()
+	sig2, err := NewSignatureFromHex(hex)
+	require.NoError(t, err)
+	require.Equal(t, sig, sig2)
+}
