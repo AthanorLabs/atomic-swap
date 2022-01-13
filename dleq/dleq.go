737d6a7a53756d79a2fa5d56fb03c2aa81d29ca0
noot <elizabethjbinks@gmail.com>
2022-01-12 22:45:23 -0500

    go fmt

--- dleq/dleq.go
+++ dleq/dleq.go
@@ -1,8 +1,8 @@
 package dleq
 
 import (
-	"crypto/ed25519"
 	"crypto/ecdsa"
+	"crypto/ed25519"
 
 	"filippo.io/edwards25519"
 	//"github.com/ethereum/go-ethereum/crypto/secp256k1"
@@ -20,33 +20,34 @@ type Secp256k1Signature [65]byte
 type Ed25519Signature [ed25519.SignatureSize]byte
 
 type PublicKeyTuple struct {
-	ed25519Key *edwards25519.Point
+	ed25519Key   *edwards25519.Point
 	secp256k1Key *ecdsa.PublicKey
 }
 
 type SignatureTuple struct {
 	ed25519Signature *Ed25519Signature
-	secp256k1Key *Secp256k1Signature
+	secp256k1Key     *Secp256k1Signature
 }
 
 type Proof struct {
 	baseCommitments []*PublicKeyTuple
 	firstChallenges [32]byte
-	sValues [2]*PublicKeyTuple
-	signatures *SignatureTuple
+	sValues         [2]*PublicKeyTuple
+	signatures      *SignatureTuple
 }
 
 type ProofSecret struct {
-	secret [32]byte
-	ed25519Key *edwards25519.Scalar
+	secret       [32]byte
+	ed25519Key   *edwards25519.Scalar
 	secp256k1Key *ecdsa.PrivateKey
 }
 
 var (
-	dleqGenBinPath = "../farcaster-dleq/target/release/dleq-gen"
+	dleqGenBinPath    = "../farcaster-dleq/target/release/dleq-gen"
 	dleqVerifyBinPath = "../farcaster-dleq/target/release/dleq-verify"
+)
 
-type FarcasterDLEq struct {}
+type FarcasterDLEq struct{}
 
 func (d *FarcasterDLEq) Prove() (*Proof, *ProofSecret, error) {
 
