afc2efa5d9c94ac2e46d0377f281db3642fb0902
noot <elizabethjbinks@gmail.com>
2022-01-22 22:36:09 -0500

    implement SwapFactory.sol

--- scripts/generate-bindings.sh
+++ scripts/generate-bindings.sh
@@ -4,3 +4,7 @@ $SOLC_BIN --abi ethereum/contracts/Swap.sol -o ethereum/abi/ --overwrite
 $SOLC_BIN --bin ethereum/contracts/Swap.sol -o ethereum/bin/ --overwrite
 abigen --abi ethereum/abi/Swap.abi --pkg swap --type Swap --out swap.go --bin ethereum/bin/Swap.bin
 mv swap.go ./swap-contract
+
+
+$SOLC_BIN --abi ethereum/contracts/SwapFactory.sol -o ethereum/abi/ --overwrite
+$SOLC_BIN --bin ethereum/contracts/SwapFactory.sol -o ethereum/bin/ --overwrite
