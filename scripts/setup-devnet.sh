82ac1812c9f8262c090290aa698664d208d4a7b3
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2022-02-13 19:06:50 +0000

    Update scripts/setup-devnet.sh

--- scripts/setup-devnet.sh
+++ scripts/setup-devnet.sh
@@ -33,19 +33,3 @@ echo "launch Bob swapd"
 
 echo "let Bob make an offer"
 ./swapcli make --min-amount 0.1 --max-amount 1 --exchange-rate 0.05 --daemon-addr=http://localhost:5002
-
-# echo "starting monero-wallet-rpc on port 18083..."
-# ./monero-x86_64-linux-gnu-v0.17.3.0/monero-wallet-rpc --rpc-bind-port 18083 --disable-rpc-login --wallet-dir ./bob-test-keys --stagenet --trusted-daemon
-
-# echo "starting monero-wallet-rpc on port 18084..."
-# ./monero-x86_64-linux-gnu-v0.17.3.0/monero-wallet-rpc --rpc-bind-port 18084 --disable-rpc-login --wallet-dir ./alice-test-keys --stagenet --trusted-daemon
-
-# open Bob's wallet (must have funds)
-# sleep 5
-# curl http://localhost:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"open_wallet","params":{"filename":"stagenet-wallet","password":""}}' -H 'Content-Type: application/json'
-
-# check balance
-# curl http://localhost:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":get_accounts","params":{}}' -H 'Content-Type: application/json'
-
-# start mining (if synced)
-# curl http://localhost:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":start_mining","params":{"threads_count":1,"do_background_mining":true,"ignore_battery":false}}' -H 'Content-Type: application/json'
