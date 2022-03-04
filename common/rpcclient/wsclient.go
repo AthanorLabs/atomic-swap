a551559ec0fd1173f5d1d5ceaf2badac345e844c
noot <elizabethjbinks@gmail.com>
2022-03-03 21:43:13 -0500

    implement --subscribe for swapcli take

--- common/rpcclient/wsclient.go
+++ common/rpcclient/wsclient.go
@@ -3,11 +3,12 @@ package rpcclient
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 
 	"github.com/gorilla/websocket"
 	logging "github.com/ipfs/go-log"
-
-	"github.com/noot/atomic-swap/common"
+	//"github.com/noot/atomic-swap/common"
 )
 
 // DefaultJSONRPCVersion ...
@@ -17,9 +18,9 @@ var log = logging.Logger("rpcclient")
 
 // WsClient ...
 type WsClient interface {
-	SubscribeSwapStatus(id uint64) (<-chan common.Stage, error)
+	SubscribeSwapStatus(id uint64) (<-chan string, error)
 	TakeOfferAndSubscribe(multiaddr, offerID string,
-		providesAmount float64) (id uint64, ch <-chan common.StageOrExitStatus, err error)
+		providesAmount float64) (id uint64, ch <-chan string, err error)
 }
 
 type wsClient struct {
@@ -28,9 +29,9 @@ type wsClient struct {
 
 // NewWsClient ...
 func NewWsClient(ctx context.Context, endpoint string) (*wsClient, error) { ///nolint:revive
-	conn, _, err := (&websocket.Dialer{}).DialContext(ctx, endpoint, nil)
+	conn, _, err := websocket.DefaultDialer.DialContext(ctx, endpoint, nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to dial endpoint: %w", err)
 	}
 
 	return &wsClient{
@@ -40,7 +41,7 @@ func NewWsClient(ctx context.Context, endpoint string) (*wsClient, error) { ///n
 
 // SubscribeSwapStatus returns a channel that is written to each time the swap's status updates.
 // If there is no swap with the given ID, it returns an error.
-func (c *wsClient) SubscribeSwapStatus(id uint64) (<-chan common.Stage, error) {
+func (c *wsClient) SubscribeSwapStatus(id uint64) (<-chan string, error) {
 	req := &Request{
 		JSONRPC: DefaultJSONRPCVersion,
 		Method:  "swap_subscribeStatus",
@@ -54,10 +55,11 @@ func (c *wsClient) SubscribeSwapStatus(id uint64) (<-chan common.Stage, error) {
 		return nil, err
 	}
 
-	respCh := make(chan common.Stage)
-	defer close(respCh)
+	respCh := make(chan string)
 
 	go func() {
+		defer close(respCh)
+
 		for {
 			_, message, err := c.conn.ReadMessage()
 			if err != nil {
@@ -83,8 +85,92 @@ func (c *wsClient) SubscribeSwapStatus(id uint64) (<-chan common.Stage, error) {
 				log.Warnf("failed to unmarshal response: %s", err)
 				break
 			}
+
+			respCh <- status.Stage
 		}
 	}()
 
 	return respCh, nil
+}
+
+func (c *wsClient) TakeOfferAndSubscribe(multiaddr, offerID string,
+	providesAmount float64) (id uint64, ch <-chan string, err error) {
+	req := &Request{
+		JSONRPC: DefaultJSONRPCVersion,
+		Method:  "net_takeOfferAndSubscribe",
+		Params: map[string]interface{}{
+			"multiaddr":      multiaddr,
+			"offerID":        offerID,
+			"providesAmount": providesAmount,
+		},
+		ID: 0,
+	}
+
+	if err := c.conn.WriteJSON(req); err != nil {
+		return 0, nil, err
+	}
+
+	// read ID from connection
+	_, message, err := c.conn.ReadMessage()
+	if err != nil {
+		return 0, nil, fmt.Errorf("failed to read websockets message: %s", err)
+	}
+
+	var resp *Response
+	err = json.Unmarshal(message, &resp)
+	if err != nil {
+		return 0, nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	if resp.Error != nil {
+		return 0, nil, fmt.Errorf("websocket server returned error: %w", resp.Error)
+	}
+
+	log.Debugf("received message over websockets: %s", message)
+	var idResp map[string]uint64
+	if err := json.Unmarshal(resp.Result, &idResp); err != nil {
+		return 0, nil, fmt.Errorf("failed to unmarshal response: %s", err)
+	}
+
+	id, ok := idResp["id"]
+	if !ok {
+		return 0, nil, errors.New("websocket response did not contain ID")
+	}
+
+	respCh := make(chan string)
+
+	go func() {
+		defer close(respCh)
+
+		for {
+			_, message, err := c.conn.ReadMessage()
+			if err != nil {
+				log.Warnf("failed to read websockets message: %s", err)
+				break
+			}
+
+			var resp *Response
+			err = json.Unmarshal(message, &resp)
+			if err != nil {
+				log.Warnf("failed to unmarshal response: %s", err)
+				break
+			}
+
+			if resp.Error != nil {
+				log.Warnf("websocket server returned error: %s", resp.Error)
+				break
+			}
+
+			log.Debugf("received message over websockets: %s", message)
+			var status *SubscribeSwapStatusResponse
+			if err := json.Unmarshal(resp.Result, &status); err != nil {
+				log.Warnf("failed to unmarshal response: %s", err)
+				break
+			}
+
+			respCh <- status.Stage
+		}
+	}()
+
+	return id, respCh, nil
 }
