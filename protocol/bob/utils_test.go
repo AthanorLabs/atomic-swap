7d2af72a00b428e7f7a58bdd8f425955057e6778
noot <36753753+noot@users.noreply.github.com>
2022-05-19 22:20:53 -0400

    check that contract code is valid before swapping (#116)

---
+++ protocol/bob/utils_test.go
@@ -0,0 +1,32 @@
+package bob
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"github.com/noot/atomic-swap/common"
+	"github.com/noot/atomic-swap/swapfactory"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheckContractCode(t *testing.T) {
+	ec, err := ethclient.Dial(common.DefaultEthEndpoint)
+	require.NoError(t, err)
+
+	pk, err := ethcrypto.HexToECDSA(common.DefaultPrivKeyBob)
+	require.NoError(t, err)
+
+	txOpts, err := bind.NewKeyedTransactorWithChainID(pk, big.NewInt(common.GanacheChainID))
+	require.NoError(t, err)
+
+	addr, _, _, err := swapfactory.DeploySwapFactory(txOpts, ec)
+	require.NoError(t, err)
+
+	err = checkContractCode(context.Background(), ec, addr)
+	require.NoError(t, err)
+}
