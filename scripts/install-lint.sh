ca3698b8f6d0c69eef992d419926ec5ac8cb8b41
noot <elizabethjbinks@gmail.com>
2022-07-26 01:21:45 -0400

    bump linter version and lint

--- scripts/install-lint.sh
+++ scripts/install-lint.sh
@@ -3,5 +3,5 @@
 GOBIN="$(go env GOPATH)/bin"
 
 if [[ ! -x "${GOBIN}/golangci-lint" ]]; then
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${GOBIN}" v1.46.2
+	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${GOBIN}" v1.47.2
 fi
