6a21c594094b3228b381a8a79c58a08af936cc72
Dmitry Holodov <787418+dimalinux@users.noreply.github.com>
2022-08-11 13:52:47 -0500

    Update go ethereum (#149)
    * Updated go-ethereum dep to 1.10.21
    * Code now installs/uses an abigen that automatically matches the version of our go-ethereum in the go.mod
    * solc was updated to 0.8.16 (in the docs, we don't install it, but anyone wanting to generate identical output needs it).
    * Minimum go version is now 1.18
    * golangci-lint updated to v1.48.0 (was needed to play nicely with go 1.18 and the new generated code)
    * dleq/cgo-dleq submodule updated to latest version

---
+++ scripts/install-abigen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Installs abigen into the user's GOPATH bin directory if it is not there already
+# or if the existing version does not match go-ethereum version in go.mod.
+
+ABIGEN="$(go env GOPATH)/bin/abigen"
+GO_MOD="$(dirname "$(readlink -f "$0")")/../go.mod"
+
+VERSION="$(grep --max-count=1 "github.com/ethereum/go-ethereum" "${GO_MOD}" | cut '--delimiter= ' --fields=2)"
+if [[ -z "${VERSION}" ]]; then
+	echo "Failed to determine correct abigen version"
+	exit 1
+fi
+
+if [[ ! -x "${ABIGEN}" ]] || [[ $("${ABIGEN}" --version) != *"${VERSION}-stable"* ]]; then
+	go install "github.com/ethereum/go-ethereum/cmd/abigen@${VERSION}"
+fi
