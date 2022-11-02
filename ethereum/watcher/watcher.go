9dd73626f21c26957744496a06f59d94a6ac642b
noot <elizabethjbinks@gmail.com>
2022-11-01 22:20:34 -0400

    begin ethereum event watcher

---
+++ ethereum/watcher/watcher.go
@@ -0,0 +1,86 @@
+package watcher
+
+import (
+	"context"
+	"math/big"
+	"time"
+
+	eth "github.com/ethereum/go-ethereum"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var checkForBlocksTimeout = time.Second
+
+type EventFilterer struct {
+	ctx         context.Context
+	ec          *ethclient.Client
+	filterQuery eth.FilterQuery
+	logCh       chan<- []ethtypes.Log
+}
+
+func NewEventFilterer(
+	ctx context.Context,
+	ec *ethclient.Client,
+	contract ethcommon.Address,
+	fromBlock *big.Int,
+	topics [][]ethcommon.Hash,
+	logCh chan<- []ethtypes.Log,
+) *EventFilterer {
+	filterQuery := eth.FilterQuery{
+		FromBlock: fromBlock,
+		Addresses: []ethcommon.Address{contract},
+		Topics:    topics,
+	}
+
+	return &EventFilterer{
+		ctx:         ctx,
+		ec:          ec,
+		filterQuery: filterQuery,
+		logCh:       logCh,
+	}
+}
+
+func (f *EventFilterer) Start() error {
+	header, err := f.ec.HeaderByNumber(f.ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case <-f.ctx.Done():
+				return
+			case <-time.After(checkForBlocksTimeout):
+			}
+
+			currHeader, err := f.ec.HeaderByNumber(f.ctx, nil)
+			if err != nil {
+				continue
+			}
+
+			if currHeader.Number.Cmp(header.Number) <= 0 {
+				// no new blocks, don't do anything
+				continue
+			}
+
+			// let's see if we have logs
+			logs, err := f.ec.FilterLogs(f.ctx, f.filterQuery)
+			if err != nil {
+				continue
+			}
+
+			if len(logs) != 0 {
+				f.logCh <- logs
+			}
+
+			// should this be currHeader + 1? ie. is the filter inclusive of the latest block?
+			f.filterQuery.FromBlock = currHeader.Number
+			header = currHeader
+		}
+	}()
+
+	return nil
+}
