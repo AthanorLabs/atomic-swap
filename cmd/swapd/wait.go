74465c804a8b79ba955c3db889835e8c14aa33e1
Dmitry Holodov <dimalinux@protonmail.com>
2022-11-03 12:25:41 -0500

    reworked swapd context handling and stop method

--- cmd/swapd/wait.go
+++ cmd/swapd/wait.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-func (d *daemon) wait() {
+func signalHandler(ctx context.Context, cancel context.CancelFunc) {
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
 	defer signal.Stop(sigc)
@@ -15,8 +16,8 @@ func (d *daemon) wait() {
 	select {
 	case <-sigc:
 		fmt.Println("signal interrupt, shutting down...")
-		d.cancel()
-	case <-d.ctx.Done():
+		cancel()
+	case <-ctx.Done():
 		fmt.Println("protocol complete, shutting down...")
 	}
 }
