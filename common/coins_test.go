c4a84800fb62b38b65437aa8e8129888dbece579
noot <elizabethjbinks@gmail.com>
2022-11-15 10:41:33 -0500

    add ERC20TokenAmount test

--- common/coins_test.go
+++ common/coins_test.go
@@ -26,3 +26,13 @@ func TestEtherAmount(t *testing.T) {
 	etherAmount := NewEtherAmount(amountUint)
 	require.Equal(t, amountUint, etherAmount.BigInt().Int64())
 }
+
+func TestERC20TokenAmount(t *testing.T) {
+	amount := float64(33.999999999)
+	wei := NewERC20TokenAmountFromDecimals(amount, 9)
+	require.Equal(t, fmt.Sprintf("%.9f", amount), fmt.Sprintf("%.9f", wei.AsStandard()))
+
+	amountUint := int64(8181)
+	tokenAmt := NewERC20TokenAmount(amountUint, 9)
+	require.Equal(t, amountUint, tokenAmt.BigInt().Int64())
+}
