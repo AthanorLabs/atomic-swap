71c652b3652d03b98ad0930990b591045da1be91
noot <elizabethjbinks@gmail.com>
2022-11-15 10:44:58 -0500

    address pr comment

--- rpcclient/wsclient/wsclient.go
+++ rpcclient/wsclient/wsclient.go
@@ -231,8 +231,10 @@ func (c *wsClient) SubscribeSwapStatus(id types.Hash) (<-chan types.Status, erro
 	return respCh, nil
 }
 
-func (c *wsClient) TakeOfferAndSubscribe(multiaddr, offerID string,
-	providesAmount float64) (ch <-chan types.Status, err error) {
+func (c *wsClient) TakeOfferAndSubscribe(
+	multiaddr, offerID string,
+	providesAmount float64,
+) (ch <-chan types.Status, err error) {
 	params := &rpctypes.TakeOfferRequest{
 		Multiaddr:      multiaddr,
 		OfferID:        offerID,
@@ -255,33 +257,8 @@ func (c *wsClient) TakeOfferAndSubscribe(multiaddr, offerID string,
 		return nil, err
 	}
 
-	readMessageFn := func() (string, error) {
-		// read resp from connection to see if there's an immediate error
-		message, err := c.read() //nolint:govet
-		if err != nil {
-			return "", fmt.Errorf("failed to read websockets message: %s", err)
-		}
-
-		var resp *rpctypes.Response
-		err = json.Unmarshal(message, &resp)
-		if err != nil {
-			return "", fmt.Errorf("failed to unmarshal response: %w", err)
-		}
-
-		if resp.Error != nil {
-			return "", fmt.Errorf("websocket server returned error: %w", resp.Error)
-		}
-
-		log.Debugf("received message over websockets: %s", message)
-		var status *rpctypes.SubscribeSwapStatusResponse
-		if err := json.Unmarshal(resp.Result, &status); err != nil {
-			return "", fmt.Errorf("failed to unmarshal swap status response: %w", err)
-		}
-
-		return status.Status, nil
-	}
-
-	status, err := readMessageFn()
+	// read resp from connection to see if there's an immediate error
+	status, err := c.readTakeOfferResponse()
 	if err != nil {
 		return nil, err
 	}
@@ -298,7 +275,7 @@ func (c *wsClient) TakeOfferAndSubscribe(multiaddr, offerID string,
 				return
 			}
 
-			status, err = readMessageFn()
+			status, err = c.readTakeOfferResponse()
 			if err != nil {
 				log.Warnf("%s", err)
 				break
@@ -307,6 +284,31 @@ func (c *wsClient) TakeOfferAndSubscribe(multiaddr, offerID string,
 	}()
 
 	return respCh, nil
+}
+
+func (c *wsClient) readTakeOfferResponse() (string, error) {
+	message, err := c.read()
+	if err != nil {
+		return "", fmt.Errorf("failed to read websockets message: %s", err)
+	}
+
+	var resp *rpctypes.Response
+	err = json.Unmarshal(message, &resp)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	if resp.Error != nil {
+		return "", fmt.Errorf("websocket server returned error: %w", resp.Error)
+	}
+
+	log.Debugf("received message over websockets: %s", message)
+	var status *rpctypes.SubscribeSwapStatusResponse
+	if err := json.Unmarshal(resp.Result, &status); err != nil {
+		return "", fmt.Errorf("failed to unmarshal swap status response: %w", err)
+	}
+
+	return status.Status, nil
 }
 
 func (c *wsClient) MakeOfferAndSubscribe(
