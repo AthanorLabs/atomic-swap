151a2af226717da8143ce6a1be1b64f24520d307
Dmitry Holodov <dimalinux@protonmail.com>
2022-11-18 11:51:28 -0600

    PR feedback: improved comments in run-integration-tests.sh

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -10,13 +10,14 @@ check-set-swap-test-data-dir
 
 RELAYER_PORT=7799
 
-# First 5 deterministic ganache keys (only 4 used at the moment)
+# The first 5 ganache keys are reserved for use by integration tests and dev swapd
+# instances. For now, we are only using 4: Alice, Bob, Charlie and the relayer.
 GANACHE_KEYS=(
 	"4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d" # Key 0
 	"6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1" # Key 1
 	"6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c" # Key 2
 	"646f1ce2fdad0e6deeeb5c7e8e5543bdde65e86029e2fd9fc169899c440a7913" # Key 3
-	"add53f9a7e588d003326d1cbf9e4a43c061aadd9bc938c843a79e7b4fd2ad743" # Key 4
+	"add53f9a7e588d003326d1cbf9e4a43c061aadd9bc938c843a79e7b4fd2ad743" # Key 4 (placeholder)
 )
 
 KEY_USERS=(
