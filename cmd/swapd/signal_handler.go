f77a1d20a2f6436ab95e060bf924398522838476
Dmitry Holodov <dimalinux@protonmail.com>
2022-11-17 23:01:27 -0600

    signal handler fixes

---
+++ cmd/swapd/signal_handler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func signalHandler(ctx context.Context, cancel context.CancelFunc) {
+	sigc := make(chan os.Signal, 1)
+	signal.Ignore(syscall.SIGHUP)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	defer func() {
+		// Hopefully, we'll exit our main() before this sleep ends, but if not remove the
+		// signal handler allowing the next signal to kill us.
+		time.Sleep(1 * time.Second)
+		signal.Stop(sigc)
+	}()
+
+	select {
+	case s := <-sigc:
+		log.Infof("Received signal %s(%d), shutting down...", s, s)
+		cancel()
+	case <-ctx.Done():
+		log.Info("Protocol complete, shutting down...")
+	}
+}
