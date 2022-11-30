55cd75e35187d4de308ff7b58363a5ad5b58441c
noot <elizabethjbinks@gmail.com>
2022-11-29 19:08:56 -0500

    break function out of other function, add comments

--- net/discovery.go
+++ net/discovery.go
@@ -86,7 +86,7 @@ func (d *discovery) start() error {
 
 	// wait to connect to bootstrap peers
 	time.Sleep(time.Second)
-	go d.advertise()
+	go d.advertiseLoop()
 
 	log.Debug("discovery started!")
 	return nil
@@ -96,53 +96,63 @@ func (d *discovery) stop() error {
 	return d.dht.Close()
 }
 
-func (d *discovery) advertise() {
+func (d *discovery) advertiseLoop() {
 	ttl := initialAdvertisementTimeout
 
-	doAdvertise := func() {
-		log.Debug("advertising in the DHT...")
-		err := d.dht.Bootstrap(d.ctx)
-		if err != nil {
-			log.Warnf("failed to bootstrap DHT: err=%s", err)
-			return
-		}
-
-		for _, provides := range d.provides {
-			ttl, err = d.rd.Advertise(d.ctx, string(provides))
-			if err != nil {
-				log.Debugf("failed to advertise in the DHT: err=%s", err)
-				ttl = tryAdvertiseTimeout
-				return
-			}
-		}
-
-		ttl, err = d.rd.Advertise(d.ctx, "")
-		if err != nil {
-			log.Debugf("failed to advertise in the DHT: err=%s", err)
-			ttl = tryAdvertiseTimeout
-			return
-		}
-
-		ttl = defaultAdvertiseTTL
-	}
+	ttl = d.advertise(ttl)
 
 	for {
 		select {
 		case <-d.advertiseCh:
-			// TODO: check current offers, as this may change (#160)
 			d.provides = []types.ProvidesCoin{types.ProvidesXMR}
-			doAdvertise()
+			ttl = d.advertise(ttl)
 		case <-time.After(ttl):
+			// the DHT clears provider records (ie. who is advertising what content)
+			// every 24 hours.
+			// so, if we don't have any offers available for 24 hours, then we are
+			// no longer present in the DHT as a provider.
+			// otherwise, we'll be present, but no offers will be sent when peers
+			// query us.
 			offers := d.offerAPI.GetOffers()
 			if len(offers) == 0 {
 				continue
 			}
 
-			doAdvertise()
+			ttl = d.advertise(ttl)
 		case <-d.ctx.Done():
 			return
 		}
 	}
+}
+
+// advertise advertises that we provide XMR in the DHT.
+// note: we only advertise that we are an XMR provider, but we don't
+// advertise our specific offers.
+// to find what our offers are, peers need to send us a QueryRequest
+// over the query subprotocol.
+func (d *discovery) advertise(ttl time.Duration) time.Duration {
+	log.Debug("advertising in the DHT...")
+	err := d.dht.Bootstrap(d.ctx)
+	if err != nil {
+		log.Warnf("failed to bootstrap DHT: err=%s", err)
+		return ttl
+	}
+
+	for _, provides := range d.provides {
+		ttl, err = d.rd.Advertise(d.ctx, string(provides))
+		if err != nil {
+			log.Debugf("failed to advertise in the DHT: err=%s", err)
+			return tryAdvertiseTimeout
+		}
+	}
+
+	ttl, err = d.rd.Advertise(d.ctx, "")
+	if err != nil {
+		log.Debugf("failed to advertise in the DHT: err=%s", err)
+		return tryAdvertiseTimeout
+	}
+
+	return defaultAdvertiseTTL
 }
 
 func (d *discovery) discover(provides types.ProvidesCoin,
