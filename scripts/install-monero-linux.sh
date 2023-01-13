1915db2ae6dfd435e4dfc353434c6e535b4ce056
Dmitry Holodov <dimalinux@protonmail.com>
2023-01-13 17:13:09 -0600

    arch detection for monero download

--- scripts/install-monero-linux.sh
+++ scripts/install-monero-linux.sh
@@ -4,7 +4,22 @@
 # deleting the monero-bin symlink or the version specific folder that it links to. This
 # script changes directories and should be executed, not sourced.
 #
-arch=macarm8
+
+os_name="$(uname)"
+arch_name="$(uname -m)"
+
+if [[ "${os_name}" ==  Linux ]] && [[ "${arch_name}" == x86_64 ]]; then
+	xmr_arch=linux64
+elif [[ "${os_name}" ==  Linux ]] && [[ "${arch_name}" == aarch64 ]]; then
+	xmr_arch=linuxarm8
+elif [[ "${os_name}" ==  macOS ]] && [[ "${arch_name}" == x86_64 ]]; then
+	xmr_arch=mac64
+elif [[ "${os_name}" ==  macOS ]] && [[ "${arch_name}" == arm64 ]]; then
+	xmr_arch=macarm8
+else
+	echo "OS=${os_name} on ARCH=${arch_name} is not supported"
+	exit 1
+fi
 
 PROJECT_ROOT="$(dirname "$(dirname "$(readlink -f "$0")")")"
 cd "${PROJECT_ROOT}"
@@ -16,7 +31,7 @@ fi
 
 set -e
 
-curl -L "https://downloads.getmonero.org/cli/${arch}" -o monero.tar.bz2
+curl -L "https://downloads.getmonero.org/cli/${xmr_arch}" -o monero.tar.bz2
 tar xjvf monero.tar.bz2
 
 # Give the architecture and version specific release dir a fixed "monero-bin" symlink
