69b357638f0e0c540fcebc8ab9d4bbbc1425925f
Dmitry Holodov <dimalinux@protonmail.com>
2023-02-01 22:15:43 -0600

    gosec rand adjustments

--- cmd/swaptester/main.go
+++ cmd/swaptester/main.go
@@ -206,7 +206,7 @@ var (
 )
 
 func getRandomExchangeRate() *coins.ExchangeRate {
-	rateFl := minExchangeRate + mrand.Float64()*(maxExchangeRate-minExchangeRate) //nolint:gosec
+	rateFl := minExchangeRate + mrand.Float64()*(maxExchangeRate-minExchangeRate)
 	rate, _, err := new(apd.Decimal).SetString(strconv.FormatFloat(rateFl, 'f', -1, 64))
 	if err != nil {
 		panic(err) // shouldn't be possible
@@ -215,7 +215,7 @@ func getRandomExchangeRate() *coins.ExchangeRate {
 }
 
 func getRndOfferAmount(xRate *coins.ExchangeRate, minXMRAmt, maxXMRAmt *apd.Decimal) (*apd.Decimal, error) {
-	randVal, err := new(apd.Decimal).SetFloat64(mrand.Float64()) //nolint:gosec
+	randVal, err := new(apd.Decimal).SetFloat64(mrand.Float64())
 	if err != nil {
 		return nil, err
 	}
@@ -413,7 +413,7 @@ func (d *daemon) takeOffer(done <-chan struct{}) {
 }
 
 func getRandomInt(max int) int {
-	i, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	i, _ := rand.Int(rand.Reader, big.NewInt(int64(max))) //nolint:gosec
 	return int(i.Int64())
 }
 
