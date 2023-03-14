b0aeaa107c21e7ff9d25b5a4f55260749705e0dd
Dmitry Holodov <dimalinux@protonmail.com>
2023-03-14 09:23:52 -0500

    added more unit tests, potential problem in signatures found

--- relayer/validate_test.go
+++ relayer/validate_test.go
@@ -2,10 +2,13 @@ package relayer
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"fmt"
 	"math/big"
 	"testing"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 
 	contracts "github.com/athanorlabs/atomic-swap/ethereum"
@@ -15,15 +18,9 @@ import (
 
 func TestValidateRelayerFee(t *testing.T) {
 	ctx := context.Background()
-
 	ec, _ := tests.NewEthClient(t)
 	key := tests.GetTakerTestKey(t)
-
-	forwarderAddr, err := contracts.DeployGSNForwarderWithKey(ctx, ec, key)
-	require.NoError(t, err)
-
-	contractAddr, _, err := contracts.DeploySwapFactoryWithKey(ctx, ec, key, forwarderAddr)
-	require.NoError(t, err)
+	swapFactoryAddr, forwarderAddr := deployContracts(t, ec, key)
 
 	type testCase struct {
 		description string
@@ -64,16 +61,69 @@ func TestValidateRelayerFee(t *testing.T) {
 		var secret [32]byte
 
 		request := &message.RelayClaimRequest{
-			SFContractAddress: contractAddr,
+			SFContractAddress: swapFactoryAddr,
 			Swap:              swap,
 			Secret:            secret[:],
 		}
 
-		err = validateClaimValues(ctx, request, ec, forwarderAddr)
+		err := validateClaimValues(ctx, request, ec, forwarderAddr)
 		if tc.expectErr != "" {
 			require.ErrorContains(t, err, tc.expectErr, tc.description)
 		} else {
 			require.NoError(t, err, tc.description)
 		}
 	}
+}
+
+func Test_validateSignature(t *testing.T) {
+	ctx := context.Background()
+	ethKey := tests.GetMakerTestKey(t)
+	claimer := crypto.PubkeyToAddress(*ethKey.Public().(*ecdsa.PublicKey))
+	ec, _ := tests.NewEthClient(t)
+	secret := [32]byte{0x1}
+	swapFactoryAddr, forwarderAddr := deployContracts(t, ec, ethKey)
+
+	swap := createTestSwap(claimer)
+	req, err := CreateRelayClaimRequest(ctx, ethKey, ec, swapFactoryAddr, forwarderAddr, swap, &secret)
+	require.NoError(t, err)
+
+	// success path
+	err = validateClaimSignature(ctx, ec, req)
+	require.NoError(t, err)
+
+	/*
+	 * WARNING, WARNING:  Why is the check below not failing?
+	 */
+	// failure path
+	for i := 0; i < 65; i++ {
+		req.Signature[i]++
+	}
+	err = validateClaimSignature(ctx, ec, req)
+	if err == nil {
+		t.Logf("FAILURE: signature above should not have validated")
+	}
+
+}
+
+func Test_validateClaimRequest(t *testing.T) {
+	ctx := context.Background()
+	ethKey := tests.GetMakerTestKey(t)
+	claimer := crypto.PubkeyToAddress(*ethKey.Public().(*ecdsa.PublicKey))
+	ec, _ := tests.NewEthClient(t)
+	secret := [32]byte{0x1}
+	swapFactoryAddr, forwarderAddr := deployContracts(t, ec, ethKey)
+
+	swap := createTestSwap(claimer)
+	req, err := CreateRelayClaimRequest(ctx, ethKey, ec, swapFactoryAddr, forwarderAddr, swap, &secret)
+	require.NoError(t, err)
+
+	// success path
+	err = validateClaimRequest(ctx, req, ec, swapFactoryAddr)
+	require.NoError(t, err)
+
+	// test failure path by passing a non-eth asset
+	asset := ethcommon.Address{0x1}
+	req.Swap.Asset = asset
+	err = validateClaimRequest(ctx, req, ec, forwarderAddr)
+	require.ErrorContains(t, err, fmt.Sprintf("relaying for ETH Asset %s is not supported", asset))
 }
