8d7e7ddb31ff776b6960be9238a85946187b5af4
elizabeth <elizabethjbinks@gmail.com>
2023-04-15 08:11:27 -0400

    remove

--- scripts/license.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-LICENSE_HEADER="// Copyright 2023 Athanor Labs (ON)\n// SPDX-License-Identifier: LGPL-3.0-only\n"
-
-find "." -type f -name "*.go" -print0 | while read -r -d $'\0' file; do
-	echo -e "$LICENSE_HEADER" | cat - "$file" >temp && mv temp "$file"
-done
