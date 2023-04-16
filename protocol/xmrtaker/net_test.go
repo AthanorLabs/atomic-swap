fd55a0176f84d39a5d59cd8f6752f8e7b2a4706a
Matt <stubbrn@protonmail.com>
2023-04-16 22:47:13 +0200

    improve InitiateProtocol unit tests

--- protocol/xmrtaker/net_test.go
+++ protocol/xmrtaker/net_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/athanorlabs/atomic-swap/coins"
+	"github.com/athanorlabs/atomic-swap/common"
 	"github.com/athanorlabs/atomic-swap/common/types"
 )
 
@@ -26,29 +27,39 @@ func newTestXMRTaker(t *testing.T) *Instance {
 	return xmrtaker
 }
 
+func initiate(xmrtaker *Instance, providesAmount *apd.Decimal, minAmount *apd.Decimal, maxAmount *apd.Decimal) (*types.Offer, common.SwapState, error) {
+	offer := types.NewOffer(coins.ProvidesETH, minAmount, maxAmount, coins.ToExchangeRate(apd.New(1, 0)), types.EthAssetETH)
+	s, err := xmrtaker.InitiateProtocol(testPeerID, providesAmount, offer)
+	return offer, s, err
+}
+
 func TestXMRTaker_InitiateProtocol(t *testing.T) {
 	a := newTestXMRTaker(t)
 	zero := new(apd.Decimal)
 	one := apd.New(1, 0)
 
 	// Provided between minAmount and maxAmount
-	offer := types.NewOffer(coins.ProvidesETH, zero, one, coins.ToExchangeRate(one), types.EthAssetETH)
-	providesAmount := apd.New(1, -1) // 0.1
-	s, err := a.InitiateProtocol(testPeerID, providesAmount, offer)
+	offer, s, err := initiate(a, apd.New(1, -1), zero, one) // 0.1
 	require.NoError(t, err)
 	require.Equal(t, a.swapStates[offer.ID], s)
 
+	// Provided with too many decimals
+	_, s, err = initiate(a, apd.New(1, -50), zero, one) // 10^-50
+	require.Error(t, err)
+	require.Equal(t, nil, s)
+
+	// Provided with a negative number
+	_, s, err = initiate(a, apd.New(-1, 0), zero, one) // -1
+	require.Error(t, err)
+	require.Equal(t, nil, s)
+
 	// Provided over maxAmount
-	offer = types.NewOffer(coins.ProvidesETH, one, one, coins.ToExchangeRate(one), types.EthAssetETH)
-	providesAmount = apd.New(2, 0) // 2
-	s, err = a.InitiateProtocol(testPeerID, providesAmount, offer)
+	_, s, err = initiate(a, apd.New(2, 0), one, one) // 2
 	require.Error(t, err)
 	require.Equal(t, nil, s)
 
 	// Provided under minAmount
-	offer = types.NewOffer(coins.ProvidesETH, one, one, coins.ToExchangeRate(one), types.EthAssetETH)
-	providesAmount = apd.New(1, -1) // 0.1
-	s, err = a.InitiateProtocol(testPeerID, providesAmount, offer)
+	_, s, err = initiate(a, apd.New(1, -1), one, one) // 0.1
 	require.Error(t, err)
 	require.Equal(t, nil, s)
 }
