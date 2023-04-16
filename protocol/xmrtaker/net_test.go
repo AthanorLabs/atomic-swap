c06ad83a50f25f743f6f85efc3df7af0e5a74a20
Matt <stubbrn@protonmail.com>
2023-04-16 20:43:41 +0200

    extend `TestXMRTaker_InitiateProtocol` unit test

--- protocol/xmrtaker/net_test.go
+++ protocol/xmrtaker/net_test.go
@@ -30,9 +30,25 @@ func TestXMRTaker_InitiateProtocol(t *testing.T) {
 	a := newTestXMRTaker(t)
 	zero := new(apd.Decimal)
 	one := apd.New(1, 0)
-	offer := types.NewOffer(coins.ProvidesETH, zero, zero, coins.ToExchangeRate(one), types.EthAssetETH)
-	providesAmount := apd.New(333, -2) // 3.33
+
+	// Provided between minAmount and maxAmount
+	offer := types.NewOffer(coins.ProvidesETH, zero, one, coins.ToExchangeRate(one), types.EthAssetETH)
+	providesAmount := apd.New(1, -1) // 0.1
 	s, err := a.InitiateProtocol(testPeerID, providesAmount, offer)
 	require.NoError(t, err)
 	require.Equal(t, a.swapStates[offer.ID], s)
+
+	// Provided over maxAmount
+	offer = types.NewOffer(coins.ProvidesETH, one, one, coins.ToExchangeRate(one), types.EthAssetETH)
+	providesAmount = apd.New(2, 0) // 2
+	s, err = a.InitiateProtocol(testPeerID, providesAmount, offer)
+	require.Error(t, err)
+	require.Equal(t, nil, s)
+
+	// Provided under maxAmount
+	offer = types.NewOffer(coins.ProvidesETH, one, one, coins.ToExchangeRate(one), types.EthAssetETH)
+	providesAmount = apd.New(1, -1) // 0.1
+	s, err = a.InitiateProtocol(testPeerID, providesAmount, offer)
+	require.Error(t, err)
+	require.Equal(t, nil, s)
 }
