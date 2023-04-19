775bdf809463f2e1fd933cea5f48bcc79ad81def
elizabeth <elizabethjbinks@gmail.com>
2023-04-19 14:40:48 -0400

    cli cleanup

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -33,7 +33,6 @@ const (
 	flagBootnodes  = "bootnodes"
 	flagRPCPort    = "rpc-port"
 	flagEnv        = "env"
-	flagLogLevel   = "log-level"
 )
 
 var log = logging.Logger("cmd")
@@ -79,7 +78,7 @@ func cliApp() *cli.App {
 				Value: "dev",
 			},
 			&cli.StringFlag{
-				Name:  flagLogLevel,
+				Name:  cliutil.FlagLogLevel,
 				Usage: "Set log level: one of [error|warn|info|debug]",
 				Value: "info",
 			},
@@ -99,55 +98,13 @@ func main() {
 	}
 }
 
-func setLogLevelsFromContext(c *cli.Context) error {
-	const (
-		levelError = "error"
-		levelWarn  = "warn"
-		levelInfo  = "info"
-		levelDebug = "debug"
-	)
-
-	level := c.String(flagLogLevel)
-	switch level {
-	case levelError, levelWarn, levelInfo, levelDebug:
-	default:
-		return fmt.Errorf("invalid log level %q", level)
-	}
-
-	setLogLevels(level)
-	return nil
-}
-
-func setLogLevels(level string) {
-	// alphabetically ordered
-	_ = logging.SetLogLevel("cmd", level)
-	_ = logging.SetLogLevel("coins", level)
-	_ = logging.SetLogLevel("common", level)
-	_ = logging.SetLogLevel("contracts", level)
-	_ = logging.SetLogLevel("cmd", level)
-	_ = logging.SetLogLevel("extethclient", level)
-	_ = logging.SetLogLevel("ethereum/watcher", level)
-	_ = logging.SetLogLevel("ethereum/block", level)
-	_ = logging.SetLogLevel("monero", level)
-	_ = logging.SetLogLevel("net", level)
-	_ = logging.SetLogLevel("offers", level)
-	_ = logging.SetLogLevel("p2pnet", level) // external
-	_ = logging.SetLogLevel("pricefeed", level)
-	_ = logging.SetLogLevel("protocol", level)
-	_ = logging.SetLogLevel("relayer", level) // external and internal
-	_ = logging.SetLogLevel("rpc", level)
-	_ = logging.SetLogLevel("txsender", level)
-	_ = logging.SetLogLevel("xmrmaker", level)
-	_ = logging.SetLogLevel("xmrtaker", level)
-}
-
 func runBootnode(c *cli.Context) error {
 	// Fail if any non-flag arguments were passed
 	if c.Args().Present() {
 		return fmt.Errorf("unknown command %q", c.Args().First())
 	}
 
-	if err := setLogLevelsFromContext(c); err != nil {
+	if err := cliutil.SetLogLevelsFromContext(c); err != nil {
 		return err
 	}
 
