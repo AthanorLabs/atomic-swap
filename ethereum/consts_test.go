feba320d7d563a88d2d66f53c985442bcb40b6df
Dmitry Holodov <dimalinux@protonmail.com>
2023-05-01 07:33:48 -0500

    increased some max gas consts based on new recorded highs

--- ethereum/consts_test.go
+++ ethereum/consts_test.go
@@ -3,6 +3,6 @@ package contracts
 // We don't deploy SwapCreator contracts or ERC20 token contracts in swaps, so
 // these constants are only compiled in for test files.
 const (
-	maxSwapCreatorDeployGas = 1062703
-	maxTestERC20DeployGas   = 798226 // using long token names or symbols will increase this
+	maxSwapCreatorDeployGas = 1062999
+	maxTestERC20DeployGas   = 798286 // using long token names or symbols will increase this
 )
